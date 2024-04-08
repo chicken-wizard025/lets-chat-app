8f38f0ea9380c936d1a371bb0bdeee92a4e0cc9e
chicken-wizard025 <127834190+chicken-wizard025@users.noreply.github.com>
2024-04-09 09:28:59 +1000

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA8upiMWQlMMP0W9uNaJhlI_fM__bfGAjs",
+  authDomain: "kwitter-9bac8.firebaseapp.com",
+  projectId: "kwitter-9bac8",
+  storageBucket: "kwitter-9bac8.appspot.com",
+  messagingSenderId: "152169948119",
+  appId: "1:152169948119:web:9e9b0fc6424632b43f6192",
+  measurementId: "G-K3229TP6M1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function addUser() {
 
     user_name = document.getElementById("user_name").value;
--- kwitter.room.js
+++ kwitter.room.js
@@ -1,52 +1,61 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBp6BRZpQI7kkMOmTJszRQIVMLCpd4MXxw",
-    authDomain: "kwitter-720ca.firebaseapp.com",
-    projectId: "kwitter-720ca",
-    storageBucket: "kwitter-720ca.appspot.com",
-    messagingSenderId: "756516389742",
-    appId: "1:756516389742:web:5d2ee9df755bc9a5f4777f",
-    measurementId: "G-PD3J0N4M5T"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  function getData() 
-  {
-        firebase.database().ref("/").on('value',
-        function(snapshot) {
-       
-         document.getElementById("output").innerHTML = "";
-         snapshot.forEach(function(childSnapshot) {
-            childKey  = childSnapshot.key;
-            Room_names = childKey;
-            console.log("Room Name - " + Room_names);
-           row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
-           document.getElementById("output").innerHTML += row;
-         });
-       });
-  }
-  getData();
+      apiKey: "AIzaSyA8upiMWQlMMP0W9uNaJhlI_fM__bfGAjs",
+      authDomain: "kwitter-9bac8.firebaseapp.com",
+      projectId: "kwitter-9bac8",
+      storageBucket: "kwitter-9bac8.appspot.com",
+      messagingSenderId: "152169948119",
+      appId: "1:152169948119:web:9e9b0fc6424632b43f6192",
+      measurementId: "G-K3229TP6M1"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+function getData() {
+      firebase.database().ref("/").on('value',
+            function (snapshot) {
 
-  function redirectToRoomName()
-  {
-        console.log(name);
-        localStorage.setItem("room_name", name);
-  }
-  function logout() {
-     localStorage.removeItem("user_name");
-     localStorage.removeItem("room_name");
-     window.location = "kwitter.html";
+                  document.getElementById("output").innerHTML = "";
+                  snapshot.forEach(function (childSnapshot) {
+                        childKey = childSnapshot.key;
+                        Room_names = childKey;
+                        console.log("Room Name - " + Room_names);
+                        row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+                        document.getElementById("output").innerHTML += row;
+                  });
+            });
+}
+getData();
 
+function redirectToRoomName() {
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "MeetUp_page.html";
+}
 
-  }
-  function addroom() 
-  {
-  room_name = document.getElementById("room_name").value;
+function logout() {
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = "index.html";
+}
 
-  firebase.database().ref("/").child(room_name).update({
-        purpose : "adding room name"
-  });
-     
-  localStorage.setItem("room_name", room_name);
+function addroom() {
+      room_name = localStorage.getItem("room_name");
+      firebase.database().ref("/").child(room_name).update({
+            purpose: "adding room name"
+      });
+      localStorage.setItem("room_name", room_name);
+}
 
- 
-  }
+function getData() {
+      firebase.database().ref("/").on('value',
+            function (snapshot) {
+                  document.getElementById("output").innerHTML = "";
+                        snapshot.forEach(function (childSnapshot) {
+                              childKey = childSnapshot.key;
+                              Room_names = childKey;
+                              row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+                        });
+                  });
+            }
+getData();
